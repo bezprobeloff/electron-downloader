0ec84e9509ef2db138b1ddf476972fd8da414165
Mikhail Belov <bezprobeloff@gmail.com>
2023-08-10 12:02:48 +0300

    feat: add dialog save file

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Button.css';
 
-
 type PropTypes = {
   disabled: boolean;
   name: string;
 };
 const Button: React.FC<PropTypes> = ({ name, disabled }) => {
+  const handleSaveFile = () => {
+    window.myAPI.save("dfdff");
+  };
   return (
-    <button type="button" id={name} className="button" disabled={disabled}>
+    <button type="button" onClick={handleSaveFile} id={name} className="button" disabled={disabled}>
       Скачать
     </button>
   );
