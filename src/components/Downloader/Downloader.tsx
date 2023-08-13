2c910ae9ef066d7bc198cc9cd83bca29d5d14933
Mikhail Belov <bezprobeloff@gmail.com>
2023-08-13 18:58:26 +0300

    feat: render component Files

--- src/components/Downloader/Downloader.tsx
+++ src/components/Downloader/Downloader.tsx
@@ -1,24 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import './Downloader.css';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 import useForm from '../../utils/hooks/useForm';
 import { INPUT_URL_FILE_ATTRIBUTES } from './constants';
+import Files from '../Files/Files';
 
 const Downloader: React.FC = () => {
   const formRef = useRef(null);
   const form = useForm(formRef);
+  const [files, setFiles] = useState([]);
   const nameInputUrl = 'urlFile';
+
+  const handleOpenFile = (pathFile: string) => {
+    window.myAPI.open(pathFile);
+  };
 
   const handleSubmit = (evt: React.FormEvent): void => {
     evt.preventDefault();
     const urlFile = form.values[nameInputUrl];
-    const options = {
-      urlFile: form.values[nameInputUrl]
+    const addFile = (savePath: string) => {
+      const newListFiles = files.filter((file, index) => index < 4);
+      setFiles([savePath, ...newListFiles]);
     };
-    window.myAPI.save(urlFile);
-    // saveFile(urlFile);
+    window.myAPI.save(urlFile, addFile);
+
     form.resetForm();
   };
 
@@ -37,6 +44,7 @@ const Downloader: React.FC = () => {
           />
           <Button name="buttonDownload" handleClick={handleSubmit} disabled={!form.isValid} />
         </form>
+        <Files files={files} handleOpenFile={handleOpenFile} />
       </div>
     </main>
   );
